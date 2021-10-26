b05c9c0fd0e5e2861ff024d663ca2a7e3f0bf885
KaiEzeckai22 <kyle.z.xxii@gmail.com>
2021-10-26 15:11:38 +0800

    Update ES2A_9 backup.cpp
    Remove Process Extract and Try Adjust

--- ES2A_9 backup.cpp
+++ ES2A_9 backup.cpp
@@ -14,7 +14,7 @@ int decimalPlaces = 0;
 int tolerance = 0;
 int digitsSize = 0;
 //String toTest[] = {"0aaKKK-7.23-24", "0aaKKK-7.23.-24", "0aaKKK-7..7-8"};
-String toTest[] = {"0aaKKK7.6e5", "0aaKKK7.66e-3", "0aaKKK7.6e8", "0aaKKK-7.6"};
+String toTest[] = {"0aaKKK42","0aaKKK4.2e","0aaKKK7.6e5", "0aaKKK7.66e-3", "0aaKKK7.6e8", "0aaKKK-7.6"};
 int toTestSize = sizeof(toTest) / sizeof(toTest[0]);
 int start = 0;
 int end = 0;
@@ -91,6 +91,9 @@ void displayData()
 void printAll(int validity, float convertedValue)
 {
     Serial.println();
+  
+        Serial.print("STRING VALUE: ");
+        Serial.println(extract);
     Serial.print("STRING IS ");
     switch (validity)
     {
@@ -106,10 +109,8 @@ void printAll(int validity, float convertedValue)
     default:
         break;
     }
-    if (validity)
+    if (1/*validity*/)
     {
-        Serial.print("STRING VALUE: ");
-        Serial.println(extract);
         Serial.print("NON EXP: ");
         Serial.println(nonExponent);
         Serial.print("EXP: ");
@@ -290,365 +291,6 @@ boolean detectCharExp(char toComp)
         }
     }
     return false;
-}
-
-int processExtract()
-{
-    // IF INVALID RETURN 0
-    // IF VALID RETURN 1
-    // IF PARTIALLY VALID RETURN 2
-    intSize = 0;
-    decimalPlaces = 0;
-    int state = 0;
-    int nonExpLen = 0;
-    bool partiality = 0;
-    bool noExponent = 0;
-    bool atDec = 0;
-    //---------------------------------------------------------------------
-    // EXP
-    //---------------------------------------------------------------------
-    // IF 'e' IS DETECTED DO SPLIT PROCESS
-    // OTHERWISE DIRECT READ AND PROCCESS
-    bool dotFound = 0;
-    bool digitDetect = 0;
-
-    if (!checkValid(extract[0]))
-    {
-        return 0;
-    }
-    if (detectChar('e'))
-    {
-        for (int i = 0; i < extractSize; i++)
-        {
-
-            if (state == 0)
-            {
-                // TRANSFER TO NEXT STATE IF 'e' IS DETECTED
-                if (extract[i] != 'e')
-                {
-                    // IF CURRENT CHARACTER IS VALID INJECT TO
-                    // CORRESPONDING ARRAY
-                    // OTHERWISE BREAK AND MARK ENTIRE STRING
-                    // INVALID
-                    if (checkValid(extract[i]))
-                    {
-                        //---------------------------------------------------------------------
-                        // NONEXP Component
-                        //---------------------------------------------------------------------
-                        nonExponent[i] = extract[i];
-                        // MARK DETECTED DIGIT
-                        if (isDigit(extract[i]))
-                        {
-                            digitDetect = 1;
-                            if (atDec)
-                            {
-                                decimalPlaces++;
-                            }
-                            else
-                            {
-                                intSize++;
-                            }
-                        }
-                        // EXCLUDE IF DOUBLE DOT
-                        if (!dotFound)
-                        {
-                            if (extract[i] == '.')
-                            {
-                                if (digitDetect)
-                                {
-                                    decimalPlaces = 0;
-                                    atDec = 1;
-                                    dotFound = 1;
-                                }
-                                else
-                                {
-                                    return 0;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (extract[i] == '.')
-                            {
-                                return 0;
-                            }
-                        }
-                        // EXCLUDE IF DOUBLE NEGATIVE
-                        if (i != 0 && (extract[i] == '-'))
-                        {
-                            return 0;
-                        }
-                        nonExpLen++;
-                    }
-                    else
-                    {
-                        return 0;
-                    }
-                }
-                else
-                {
-                    state = 1;
-                }
-            }
-            else
-            {
-                // NO FILTER FOR POST PROCESS
-                exponent[i - nonExpLen - 1] = extract[i];
-            }
-        }
-        // PRE-PROCESS SIZE OR ARRAYS FOR POST PROCESSING
-        nonExponentSize = nonExpLen;
-        exponentSize = (end - start - 1) - nonExpLen;
-    }
-    else
-    {
-
-        //---------------------------------------------------------------------
-        // NONEXP IN CONSRUCTION
-        //---------------------------------------------------------------------
-        // IF NO EXPONENT MARK IT THEN
-        // SET SIZES ACCORDINGLY
-        // PROCESSING NON EXPONENT
-        noExponent = 1;
-        nonExponentSize = extractSize;
-        bool expFound = 0;
-        // IF START IS INVALID EXIT AND MARK INVALID
-        if (!checkValid(extract[0]))
-        {
-            return 0;
-        }
-        for (int i = 0; i < extractSize; i++)
-        {
-            // IF DIGIT MARK DETECTED OTHERWISE IF NOT RETURN 0
-            // FILTERS THE ".1" INVALIDITY
-            if (isDigit(extract[i]))
-            {
-                digitDetect = 1;
-                // STOP COUNTING INT AND DECIMAL PLACES AT START OF PARTIALITY
-                if (!partiality)
-                {
-                    if (atDec)
-                    {
-                        decimalPlaces++;
-                    }
-                    else
-                    {
-                        intSize++;
-                    }
-                }
-            }
-            else
-            {
-                if (!expFound)
-                {
-                    if (extract[i] == 'e')
-                    {
-                        expFound = 1;
-                    }
-                }
-                if (!dotFound)
-                {
-                    if (extract[i] == '.')
-                    {
-                        if (digitDetect)
-                        {
-                            decimalPlaces = 0;
-                            atDec = 1;
-                            dotFound = 1;
-                        }
-                        else
-                        {
-                            // ".1" TRIGGER INVALID
-                            return 0;
-                        }
-                    }
-                }
-                else
-                {
-                    // DETECT DOUBLED DOTS
-                    if (extract[i] == '.')
-                    {
-                        partiality = 1;
-                    }
-                }
-                // DETECT DOUBLE NEGATIVES
-                if (i != 0 && (extract[i] == '-'))
-                {
-                    partiality = 1;
-                }
-            }
-        }
-        // FILTER THE IGNORED PART
-        int filterStat = 0;
-        int filtLen2 = 0;
-        if (!partiality)
-        {
-            for (int i = 0; i < nonExponentSize; i++)
-            {
-                // IF THERE ARE INVALID PARTS MARK AS PARTIALLY VALID
-                if (!checkValid(nonExponent[i]))
-                {
-                    partiality = 1;
-                    break;
-                }
-            }
-        }
-        // IF PARTIALITY IS CONFIRMED
-        // TRANSFER INVALID DATA TO IGNORED
-        // ERASE INVALID DATA FROM NONEXPONENT ARRAY
-        dotFound = 0;
-        bool doubleDot = 0;
-        if (partiality)
-        {
-            for (int i = 0; i < nonExponentSize; i++)
-            {
-                if (filterStat == 0)
-                {
-                    if (checkValid(nonExponent[i]))
-                    {
-                        // DOUBLE DOT FILTER
-                        if (!dotFound)
-                        {
-                            if (nonExponent[i] == '.')
-                            {
-                                dotFound = 1;
-                            }
-                        }
-                        else
-                        {
-                            if (nonExponent[i] == '.')
-                            {
-                                ignoredSize++;
-                                ignored[0] = nonExponent[i];
-                                nonExponent[i] = '\0';
-                                filterStat = 1;
-                                continue;
-                            }
-                        }
-                        // DOUBLE NEGATIVE FILTER
-                        if (i != 0 && (nonExponent[i] == '-'))
-                        {
-                            ignoredSize++;
-                            ignored[0] = nonExponent[i];
-                            nonExponent[i] = '\0';
-                            filterStat = 1;
-                            continue;
-                        }
-                        filtLen2++;
-                    }
-                    else
-                    {
-                        ignoredSize++;
-                        ignored[0] = nonExponent[i];
-                        nonExponent[i] = '\0';
-                        filterStat = 1;
-                    }
-                }
-                else
-                {
-                    // REFACTOR DECIMAL PLACES
-                    ignoredSize++;
-                    ignored[i - filtLen2] = nonExponent[i];
-                    nonExponent[i] = '\0';
-                }
-            }
-            if (nonExponentSize - ignoredSize > 0)
-            {
-                nonExponentSize = nonExponentSize - ignoredSize;
-            }
-        }
-        exponentSize = 0;
-    }
-    //---------------------------------------------------------------------
-    // EXP POST PROCESS
-    //---------------------------------------------------------------------
-
-    //---------------------------------------------------------------------
-    // EXP with e not empty expo
-    //---------------------------------------------------------------------
-    // PROCESSING THE EXPONENT
-    // DETECT IF EXPONENT IS EMPTY SET
-    // AND IT STILL WENT THROUGH 'e' DETECT
-    if (exponentSize == 0 && (!noExponent))
-    {
-        ignored[0] = 'e';
-        ignoredSize = 1;
-        exponentSize = 0;
-        partiality = 1;
-    }
-    // DETECT IF EXPONENT HAS '.'
-    // AND THUS THE ENTIRE 'e+any' WILL BE IGNORED
-    if (detectCharExp('.'))
-    {
-        ignored[0] = 'e';
-        partiality = 1;
-        ignoredSize++;
-        for (int i = 1; i < exponentSize + 1; i++)
-        {
-            ignored[i] = exponent[i - 1];
-            exponent[i - 1] = '\0';
-            ignoredSize++;
-        }
-    }
-    else
-    {
-        //---------------------------------------------------------------------
-        // EXP with e empty expo
-        //---------------------------------------------------------------------
-        // IF IT HAS 'e' BUT IS NOT EMPTY
-        // FILTER THE IGNORED PART
-        int stateA = 0;
-        int filtLen = 0;
-        for (int i = 0; i < exponentSize; i++)
-        {
-            // IF THERE ARE INVALID PARTS MARK AS PARTIALLY VALID
-            if (!checkValidExp(exponent[i]))
-            {
-                partiality = 1;
-                break;
-            }
-        }
-        // IF PARTIALITY IS CONFIRMED
-        // TRANSFER INVALID DATA TO IGNORED
-        // ERASE INVALID DATA FROM EXPONENT ARRAY
-        if (partiality)
-        {
-            for (int i = 0; i < exponentSize; i++)
-            {
-                if (stateA == 0)
-                {
-                    if (checkValidExp(exponent[i]))
-                    {
-                        filtLen++;
-                    }
-                    else
-                    {
-                        ignoredSize++;
-                        ignored[0] = exponent[i];
-                        exponent[i] = '\0';
-                        stateA = 1;
-                    }
-                }
-                else
-                {
-                    ignoredSize++;
-                    ignored[i - filtLen] = exponent[i];
-                    exponent[i] = '\0';
-                }
-            }
-            if (exponentSize - ignoredSize > 0)
-            {
-                exponentSize = exponentSize - ignoredSize;
-            }
-        }
-    }
-
-    // RETURN IF VALID OR PARTIALLY VALID
-    if (!partiality)
-    {
-        return 1;
-    }
-    return 2;
 }
 
 bool checkValid(char toCheck)
@@ -850,11 +492,11 @@ float convertExtended()
 
 int extractionProcess()
 {
-  intSize = 0;
-  decimalPlaces = 0;
-  nonExponentSize = 0;
-  exponentSize = 0;
-  digitsSize =0;
+    intSize = 0;
+    decimalPlaces = 0;
+    nonExponentSize = 0;
+    exponentSize = 0;
+    digitsSize = 0;
     int state = 0;
     bool dotFound = 0;
     bool nonExpFound = 0;
@@ -867,7 +509,7 @@ int extractionProcess()
     // DETECT INVALID START (err,a.b,a23)
     if (!checkValid(extract[0]))
     {
-        return -1;
+        return 0;
     }
 
     for (int i = 0; i < extractSize; i++)
@@ -899,7 +541,7 @@ int extractionProcess()
             }
             else
             {
-                return -1;
+                return 0;
             }
 
             break;
@@ -909,27 +551,32 @@ int extractionProcess()
                 // DETECT IF NO NUMBER YET THUS FILTER (.12 ==> x)
                 if (!nonExpFound)
                 {
-                    return -1;
+                    return 0;
                 }
+              if(state==0){
+
                 dotFound = 1;
                 state = 1;
+              }
+              
             }
             else
             {
                 partiality = 1;
                 state = 3;
             }
-            // DETECT DECIMAL POINT AFTER E (e.12)
-            if (eFound)
+            if (state==2)
             {
-                return -1;
+                return 0;
             }
             break;
         case 'e':
             if (!eFound)
             {
+              if(state==2){
                 eFound = 1;
                 state = 2;
+              }
             }
             else
             {
@@ -960,7 +607,7 @@ int extractionProcess()
         case 1: // NON EXPONENT DECIMAL
             if (checkValid(extract[i]))
             {
-                nonExponent[i - (negFound + intSize + dotFound)] = extract[i]; // NEEDS SHIFT i @ nonExponent
+                nonExponent[i - (negFound + intSize + dotFound) -1 ] = extract[i]; // NEEDS SHIFT i @ nonExponent
                 decimalPlaces++;
                 nonExponentSize++;
             }
@@ -973,7 +620,7 @@ int extractionProcess()
         case 2:
             if (checkValid(extract[i]))
             {
-                exponent[i - (negFound + intSize + dotFound + decimalPlaces)] = extract[i]; // NEEDS SHIFT i @ exponent
+                exponent[i - (negFound + intSize + dotFound + decimalPlaces) -1 ] = extract[i]; // NEEDS SHIFT i @ exponent
                 exponentSize++;
             }
             else
@@ -983,7 +630,7 @@ int extractionProcess()
             }
             break;
         case 3:
-            ignored[i - (negFound + intSize + dotFound + decimalPlaces + exponentSize)] = extract[i]; // NEEDS SHIFT i @ ignored
+            ignored[i - (negFound + intSize + dotFound + decimalPlaces + exponentSize) -1 ] = extract[i]; // NEEDS SHIFT i @ ignored
             ignoredSize++;
             break;
 
